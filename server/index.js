cb92a35727339bfffa55a54923df5b8511c4bc00
rawannmmohamed <rawanmelkhodary@gmail.com>
2025-08-14 22:15:08 +0300

    `Update server port and logging in index.js`

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log('JSON Server is running on port ' + port);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
+
